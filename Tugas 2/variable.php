f9cbee956a6d871965b275bd836d07ee20d7eaa5
farisulhaq <faries089@gmail.com>
2022-03-08 22:49:31 +0700

    menambah file variable.php

---
+++ Tugas 2/variable.php
@@ -0,0 +1,28 @@
+<?php  
+    // Variable untuk menampung angka
+    $angka1 = 5;
+    $angka2 = 10;
+    // variable untuk penjumlahan
+    $penjumlahan = $angka1 + $angka2;
+    // variable untuk pengurangan
+    $pengurangan = $angka2 - $angka1;
+    // variable untuk perkalian
+    $perkalian = $angka1 * $angka2;
+    // variable untuk pembagian
+    $pembagian = $angka2 / $angka1;
+    // main
+    echo "angka 1 = $angka1";
+    echo "<br>";
+    echo "angka 2 = $angka2";
+    echo "<br>";
+    // print penjumlahan
+    echo "angka 1 + angka 2 = $penjumlahan";
+    echo "<br>";
+    // print pengurangan
+    echo "angka 2 - angka 1 = $pengurangan";
+    echo "<br>";
+    // print perkalian
+    echo "angka 1 * angka 2 = $perkalian";
+    echo "<br>";
+    // print pembagian
+    echo "angka 2 / angka 1 = $pembagian";
