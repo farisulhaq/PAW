0b868dfdb3b064fd30863a4b8ef0977956c15aec
farisulhaq <faries089@gmail.com>
2022-03-08 23:06:58 +0700

    menambah file fungsi.php

---
+++ Tugas 2/fungsi.php
@@ -0,0 +1,13 @@
+<?php  
+    // fungsi mahasiswa
+    function mahasiswa($nama, $nim)
+    {
+        echo "Nama = " . $nama ."<br>NIM = " . $nim;
+        echo "<br>";
+    }
+    // main
+    mahasiswa("Ahmad Farisul Haq", "200411100171");
+    mahasiswa("Arifatul Maghfirah", "200411100201");
+    mahasiswa("Agastia Devi Wardani", "200441100145");
+?>
+
